414de7ad0efce0341f62f8aaef093b20b73706df
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-01-01 11:13:42 +0600

    Update p1929.js

--- p1929.js
+++ p1929.js
@@ -1,3 +1,4 @@
+// this code is committed by imdadulahmed1593
 // 1929. Concatenation of Array
 
 // Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
