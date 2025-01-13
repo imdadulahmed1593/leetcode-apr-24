d7e84342563e15c5ef192f2984de63ecb4b10fe5
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-01-14 00:37:46 +0600

    Update p1.js

--- p1.js
+++ p1.js
@@ -3,7 +3,7 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
 
-// the intuition
+// the intuition would be
 // 1. for an entry, I just have to check if target - entry exists in the array
 
 // algorithm
