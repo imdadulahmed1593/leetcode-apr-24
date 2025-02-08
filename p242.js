7e4305f9f86e0500cb115298ccbc859724938331
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-02-08 22:34:59 +0600

    p242 modified

--- p242.js
+++ p242.js
@@ -10,29 +10,29 @@
 // 2. if yes, then check if the two strings have the same character frequency
 // 3. if yes, then the two strings are anagrams
 
-const isAnagram = function (s, t) {
-  if (s.length !== t.length) {
-    return false;
-  }
+// const isAnagram = function (s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
 
-  let fc1 = {};
-  let fc2 = {};
+//   let fc1 = {};
+//   let fc2 = {};
 
-  for (const char of s) {
-    fc1[char] = (fc1[char] || 0) + 1;
-  }
+//   for (const char of s) {
+//     fc1[char] = (fc1[char] || 0) + 1;
+//   }
 
-  for (const char of t) {
-    fc2[char] = (fc2[char] || 0) + 1;
-  }
+//   for (const char of t) {
+//     fc2[char] = (fc2[char] || 0) + 1;
+//   }
 
-  for (const key in fc1) {
-    if (fc1[key] !== fc2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+//   for (const key in fc1) {
+//     if (fc1[key] !== fc2[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 // var isAnagram = function (s, t) {
 //   if (s.length !== t.length) return false;
@@ -48,13 +48,33 @@ const isAnagram = function (s, t) {
 //   }
 
 //   for (let char in freCounter1) {
-//     if (!(char in freCounter2)) return false;
+//     if (!(char in freCounter2)) return false; //eta na dileo hoy asole
 //     if (freCounter1[char] !== freCounter2[char]) return false;
 //   }
 
 //   return true;
 // };
 
-console.log(isAnagram("anagram", "nagaram")); // true
-console.log(isAnagram("cat", "car")); // false
+// "colt steels method"
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+
+  const lookup = {};
+  for (let char of s) {
+    lookup[char] = (lookup[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    let letter = t[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+// console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("catt", "cata")); // false
 console.log(isAnagram("a", "ab")); // false
