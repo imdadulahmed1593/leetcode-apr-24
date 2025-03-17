150fe3f9195db67e9e8abb6a5c1230f718fa0d82
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-03-17 22:03:52 +0600

    p2206 done

---
+++ p2206.js
@@ -0,0 +1,47 @@
+// 2206. Divide Array Into Equal Pairs
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given an integer array nums consisting of 2 * n integers.
+
+// You need to divide nums into n pairs such that:
+
+// Each element belongs to exactly one pair.
+// The elements present in a pair are equal.
+// Return true if nums can be divided into n pairs, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [3,2,3,2,2,2]
+// Output: true
+// Explanation:
+// There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+// If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Explanation:
+// There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var divideArray = function (nums) {
+  let counter = new Map();
+
+  for (let num of nums) {
+    counter.set(num, (counter.get(num) || 0) + 1);
+  }
+
+  for (let count of counter.values()) {
+    if (count % 2 !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
