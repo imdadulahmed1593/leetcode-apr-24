6e2114a924d575d05fb3de38900efe63715e1543
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-04-20 10:12:12 +0600

    Update p21.js

--- p21.js
+++ p21.js
@@ -43,3 +43,26 @@ const mergeTwoLists = function (list1, list2) {
 
   return dummy.next;
 };
+
+
+// python implementation
+// class Solution:
+    // def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        // dummy = prev = ListNode(0)
+
+        // p1, p2 = list1, list2
+        // while p1 and p2:
+            // if p1.val < p2.val:
+               //  prev.next = p1
+               //  p1 = p1.next
+            // else:
+               //  prev.next = p2
+               //  p2 = p2.next
+            // prev = prev.next
+
+        // if p1:
+            // prev.next = p1
+        // else:
+           //  prev.next = p2
+
+        // return dummy.next
