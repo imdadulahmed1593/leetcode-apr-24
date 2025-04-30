4d1714256a255f4c48e8944467c8c5361656cac0
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-04-30 23:31:58 +0600

    p11 updated

--- p11.js
+++ p11.js
@@ -34,3 +34,26 @@ var maxArea = function (height) {
 
   return maxArea;
 };
+
+// python implementation
+// class Solution(object):
+//     def maxArea(self, height):
+//         """
+//         :type height: List[int]
+//         :rtype: int
+//         """
+//         left = 0
+//         right = len(height) - 1
+//         max_area = 0
+
+//         while left < right:
+//             width = right - left
+//             current_area = min(height[left], height[right]) * width
+//             max_area = max(max_area, current_area)
+
+//             if height[left] < height[right]:
+//                 left += 1
+//             else:
+//                 right -= 1
+
+//         return max_area
