d1fb97a9975734ec122984976d4f6e71d626aa09
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-05-15 11:05:30 +0600

    p448 done

---
+++ p448.js
@@ -0,0 +1,30 @@
+// 448. Find All Numbers Disappeared in an Array
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  let freqCounter = {};
+  let resArr = [];
+  for (let num of nums) {
+    freqCounter[num] = (freqCounter[num] || 0) + 1;
+  }
+  for (let i = 1; i <= nums.length; i++) {
+    if (!freqCounter[i]) resArr.push(i);
+  }
+  return resArr;
+};
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5,6]
+console.log(findDisappearedNumbers([1, 1])); // [2]
